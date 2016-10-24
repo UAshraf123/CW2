f0509d66e5d4682bf1f3aea8d2994d116918554c
UAshraf123 <40096834@live.napier.ac.uk>
2016-10-25 00:48:41 +0100

    Update index.py
    added links for the rest of the pages.

--- src/index.py
+++ src/index.py
@@ -17,6 +17,18 @@ def DC5():
 def Supra():
   return render_template('Supra.html')
 
+@app.route("/S13/")
+def S13():
+  return render_template('S13.html')
+
+@app.route("/S15/")
+def S15():
+  return render_template('S15.html')
+
+@app.route("/Chaser/")
+def Chaser():
+  return render_template('Chaser.html')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
