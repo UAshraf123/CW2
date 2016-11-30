82ab1a108d16d06e098dea3a65ccc638b4399aff
UAshraf123 <40096834@live.napier.ac.uk>
2016-11-30 03:43:25 +0000

    Update test.py

--- src/test.py
+++ src/test.py
@@ -7,7 +7,7 @@ class FlaskTestCase(unittest.TestCase):
     #ensure that flask is set up correctly
     def test_index(self):
         tester = index.test_client(self)
-        response = tester.get('/', content_type='html/text')
+        response = tester.get('/index', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
