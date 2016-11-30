c218e33ce845c395a01fbdd1a8b8fc4a80505f49
UAshraf123 <40096834@live.napier.ac.uk>
2016-11-30 03:48:27 +0000

    Update test.py

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ import unittest
 class FlaskTestCase(unittest.TestCase):
 
     def test_login_page_loads(self):
-        tester = app.test_client(self)
+        tester = index.test_client(self)
         response = tester.get('/DC2', content_type='html/text')
         self.assertTrue(b'integra' in response.data)
 
