8ef2e06c41d4f1f3856e9697cbb74dc8fd856f3a
UAshraf123 <40096834@live.napier.ac.uk>
2016-11-30 04:42:22 +0000

    Update test.py
    added further testing, this time for HomePage.

--- src/test.py
+++ src/test.py
@@ -9,7 +9,14 @@ class FlaskTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+        
+    #this ensures that the correct page loads (homepage in this case).
+    def test_index_homepage(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertTrue(b'Welcome to JDM Cars' in response.data)
 
+        
           
         
 if __name__ == '__main__':
