d084f2fabd17e6c0ce83d831a4f5830f421c1b13
UAshraf123 <40096834@live.napier.ac.uk>
2016-11-30 05:04:12 +0000

    Update test.py
    Test file hopefully completed.

--- src/test.py
+++ src/test.py
@@ -28,9 +28,41 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/DC5', content_type='html/text')
         self.assertTrue(b'Honda Integra DC5 (year 01-06)' in response.data)
         
-
+    #this ensures that the correct page loads (Supra page in this case).
+    def test_index_Supra(self):
+        tester = app.test_client(self)
+        response = tester.get('/Supra/', content_type='html/text')
+        self.assertTrue(b'Toyota Supra (year 93-02)' in response.data)
         
-
+    #this ensures that the correct page loads (S13 page in this case).
+    def test_index_S13(self):
+        tester = app.test_client(self)
+        response = tester.get('/S13/', content_type='html/text')
+        self.assertTrue(b'Nissan S13 (year 89-95)' in response.data)
+        
+    #this ensures that the correct page loads (S15 page in this case).
+    def test_index_S15(self):
+        tester = app.test_client(self)
+        response = tester.get('/S15/', content_type='html/text')
+        self.assertTrue(b'Nissan S15 (year 99-02)' in response.data)
+        
+    #this ensures that the correct page loads (Chaser page in this case).
+    def test_index_Chaser(self):
+        tester = app.test_client(self)
+        response = tester.get('/Chaser/', content_type='html/text')
+        self.assertTrue(b'Toyota Chaser (year 96-01)' in response.data)
+        
+    #this ensures that the correct page loads (Map page in this case).
+    def test_index_Map(self):
+        tester = app.test_client(self)
+        response = tester.get('/Map/', content_type='html/text')
+        self.assertTrue(b'Japan on Google Maps' in response.data)
+        
+    #this ensures that the correct page loads (error404 page in this case).
+    def test_index_404(self):
+        tester = app.test_client(self)
+        response = tester.get('/404/', content_type='html/text')
+        self.assertTrue(b'OOPS!!!! PAGE NOT FOUND!! PLEASE RETURN TO HOMEPAGE BY CLICKING ON THE HEADER, OTHERWISE CLICK ONE OF THE TABS ABOVE =]' in response.data)
         
           
         
