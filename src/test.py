9c3ac8480959f7fda93fc939318eee0d1c36a98a
UAshraf123 <40096834@live.napier.ac.uk>
2016-11-30 03:46:54 +0000

    Update test.py

--- src/test.py
+++ src/test.py
@@ -5,9 +5,10 @@ import unittest
 class FlaskTestCase(unittest.TestCase):
 
 def test(self):
-        tester = app.test_client(self)
-        response = tester.get('/DC2', content_type='html/text')
-        self.assertTrue(b'Integra' in response.data)
+tester = app.test_client(self)
+response = tester.get('/DC2', content_type='html/text')
+self.assertTrue(b'Integra' in response.data)
+          
         
 if __name__ == '__main__':
     unittest.main()
