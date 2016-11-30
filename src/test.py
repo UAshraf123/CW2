9f795ea511d47f7d3d356e715522c054ed7ee326
UAshraf123 <40096834@live.napier.ac.uk>
2016-11-30 03:38:51 +0000

    Update test.py

--- src/test.py
+++ src/test.py
@@ -9,3 +9,6 @@ class FlaskTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
