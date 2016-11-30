fb559e4e4aba8578ce7893ce31a4944d69639eda
UAshraf123 <40096834@live.napier.ac.uk>
2016-11-30 04:53:21 +0000

    Update test.py

--- src/test.py
+++ src/test.py
@@ -22,17 +22,7 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/DC2', content_type='html/text')
         self.assertTrue(b'Honda Integra DC2 (year 96-00)' in response.data)
         
-    #this ensures that the correct page loads (DC5 page in this case).
-    def test_index_DC5(self):
-        tester = app.test_client(self)
-        response = tester.get('/DC5', content_type='html/text')
-        self.assertTrue(b'Honda Integra DC5 (year 01-06)' in response.data)      
-        
-    #this ensures that the correct page loads (Supra page in this case).
-    def test_index_Supra(self):
-        tester = app.test_client(self)
-        response = tester.get('/Supra', content_type='html/text')
-        self.assertTrue(b'Toyota Supra (year 93-02)' in response.data)
+
         
 
         
