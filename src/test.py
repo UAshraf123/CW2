79d54ab184e2d3443264cef3a3f239bf49b6c054
UAshraf123 <40096834@live.napier.ac.uk>
2016-11-30 04:57:27 +0000

    Update test.py
    fixed issue, small syntax error.

--- src/test.py
+++ src/test.py
@@ -22,6 +22,12 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/DC2/', content_type='html/text')
         self.assertTrue(b'Honda Integra DC2 (year 96-00)' in response.data)
         
+    #this ensures that the correct page loads (DC5 page in this case).
+    def test_index_DC5(self):
+        tester = app.test_client(self)
+        response = tester.get('/DC5', content_type='html/text')
+        self.assertTrue(b'Honda Integra DC5 (year 01-06)' in response.data)
+        
 
         
 
